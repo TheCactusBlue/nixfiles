af05626f455f01a412dbc8d886717afa38da1ff8
CactusBlue <thecactusblue@gmail.com>
2025-07-30 14:07:42 -0700

    move locales

--- systems/academy-city.nix
+++ systems/academy-city.nix
@@ -13,6 +13,7 @@
     ../games/hoyoverse.nix
 
     ../fonts.nix
+    ../locale.nix
   ];
 
   nix.settings.experimental-features = [
@@ -21,18 +22,6 @@
   ];
   networking.hostName = "academy-city"; # Define your hostname.
   time.timeZone = "America/Los_Angeles";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   programs.zsh.enable = true;
   programs.hyprland.enable = true; # Need for global hyprland
