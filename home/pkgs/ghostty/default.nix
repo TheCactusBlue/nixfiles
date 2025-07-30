fa595362fd45cb4ba05382ec5c631f3b6e3829b4
CactusBlue <thecactusblue@gmail.com>
2025-07-30 13:49:26 -0700

    factor themes

--- home/pkgs/ghostty/default.nix
+++ home/pkgs/ghostty/default.nix
@@ -1,30 +1,15 @@
 { config, pkgs, ... }:
+let
+  theme = import ../../../themes/tokyo-night.nix;
 {
   programs.ghostty = {
     enable = true;
     settings = {
       window-padding-x = 12;
       window-padding-y = 12;
-      palette = [
-        "0=#15161e"
-        "1=#f7768e"
-        "2=#9ece6a"
-        "3=#e0af68"
-        "4=#7aa2f7"
-        "5=#bb9af7"
-        "6=#7dcfff"
-        "7=#a9b1d6"
-        "8=#414868"
-        "9=#ff899d"
-        "10=#9fe044"
-        "11=#faba4a"
-        "12=#8db0ff"
-        "13=#c7a9ff"
-        "14=#a4daff"
-        "15=#c0caf5"
-      ];
-      background = "#1a1b26";
-      foreground = "#c0caf5";
+      palette = attrValues (mapattrs (name: value: name + "=" + value) theme.ansi);
+      background = theme.background;
+      foreground = theme.foreground;
       cursor-color = "#c0caf5";
       selection-background = "#283457";
       selection-foreground = "#c0caf5";
