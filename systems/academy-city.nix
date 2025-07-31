89fe7721d4edea022214562d20db422277dec3f8
TheCactusBlue <thecactusblue@gmail.com>
2025-07-30 22:37:17 -0700

    add openrgb

--- systems/academy-city.nix
+++ systems/academy-city.nix
@@ -3,7 +3,6 @@
   imports = [
     ./desktop-base.nix
     ../drivers/nvidia.nix
-    ../drivers/rgb.nix
     ../networking/host-block.nix
 
     # users
@@ -18,4 +17,5 @@
   programs.zsh.enable = true;
   programs.hyprland.enable = true; # Need for global hyprland
   programs.firefox.enable = true;
+  services.hardware.openrgb.enable = true;
 }
