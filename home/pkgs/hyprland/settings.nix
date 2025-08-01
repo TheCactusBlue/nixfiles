9a9abaced0c06396da04584b5ae9c4094035a18f
TheCactusBlue <thecactusblue@gmail.com>
2025-08-01 16:36:08 -0700

    more

--- home/pkgs/hyprland/settings.nix
+++ home/pkgs/hyprland/settings.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
 {
   wayland.windowManager.hyprland.settings = {
     # Monitor configuration
@@ -174,31 +180,18 @@
       "$mainMod, right, movefocus, r"
       "$mainMod, up, movefocus, u"
       "$mainMod, down, movefocus, d"
-
-      # Workspace switching
-      "$mainMod, 1, workspace, 1"
-      "$mainMod, 2, workspace, 2"
-      "$mainMod, 3, workspace, 3"
-      "$mainMod, 4, workspace, 4"
-      "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
+    ]
+    ++ lists.flatten (
+      genList (x: [
+        "$mainMod, ${toString (x + 1)}, workspace, ${toString (x + 1)}"
+        "$mainMod SHIFT, ${toString (x + 1)}, movetoworkspace, ${toString (x + 1)}"
+      ]) 9
+    )
+    ++ [
       "$mainMod, 0, workspace, 10"
-
-      # Move to workspace
-      "$mainMod SHIFT, 1, movetoworkspace, 1"
-      "$mainMod SHIFT, 2, movetoworkspace, 2"
-      "$mainMod SHIFT, 3, movetoworkspace, 3"
-      "$mainMod SHIFT, 4, movetoworkspace, 4"
-      "$mainMod SHIFT, 5, movetoworkspace, 5"
-      "$mainMod SHIFT, 6, movetoworkspace, 6"
-      "$mainMod SHIFT, 7, movetoworkspace, 7"
-      "$mainMod SHIFT, 8, movetoworkspace, 8"
-      "$mainMod SHIFT, 9, movetoworkspace, 9"
       "$mainMod SHIFT, 0, movetoworkspace, 10"
-
+    ]
+    ++ [
       # Special workspace
       "$mainMod, S, togglespecialworkspace, magic"
       "$mainMod SHIFT, S, movetoworkspace, special:magic"
