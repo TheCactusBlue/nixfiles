79408bf2c57004eedd3dfb750f985da6fa89098b
TheCactusBlue <thecactusblue@gmail.com>
2025-09-07 20:51:08 -0700

    add claude code ext

--- home/pkgs/vscode/default.nix
+++ home/pkgs/vscode/default.nix
@@ -22,6 +22,7 @@ in
           ms-azuretools.vscode-docker
           bbenoist.nix
           jnoortheen.nix-ide
+          vscode-extensions.anthropic.claude-code
 
           # Javascript, TypeScript and CSS
           esbenp.prettier-vscode
