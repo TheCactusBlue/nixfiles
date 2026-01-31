18d7eb3e7f2d15f2c36864705024d41d1f6c7dad
TheCactusBlue <thecactusblue@gmail.com>
2026-01-31 14:35:08 -0800

    patch

--- home/pkgs/claude-code/default.nix
+++ home/pkgs/claude-code/default.nix
@@ -8,8 +8,56 @@
   nixpkgs.overlays = [ inputs.claude-code.overlays.default ];
   programs.claude-code = {
     enable = true;
+    settings = {
+      model = "opus";
+
+      allow = [
+        "WebSearch"
+      ]
+      ++ map (cmd: "Bash(${cmd})") [
+        "find:*"
+        "rg:*"
+        "echo:*"
+        "grep:*"
+        "ls:*"
+        "cat:*"
+        "sed:*"
+        "source:*"
+        "tree:*"
+        "tail:*"
+
+        "gh pr list:*"
+        "gh pr view:*"
+        "gh pr diff:*"
+        "gh api user:*"
+        "gh repo view:*"
+        "gh issue view:*"
+        "git branch --show-current:*"
+        "git diff:*"
+        "git status:*"
+        "git rev-parse:*"
+        "git push:*"
+        "git log:*"
+      ]
+      ++ map (site: "WebFetch(domain:${site})") [
+        "docs.rs"
+        "github.com"
+        "raw.githubusercontent.com"
+        "openai.com"
+        "anthropic.com"
+        "docs.anthropic.com"
+      ];
+
+      sandbox = {
+        enabled = true;
+        autoAllowBashIfSandboxed = true;
+      };
+    };
+
   };
   home.packages = with pkgs; [
+    bubblewrap
+    socat
     ripgrep
   ];
 }
