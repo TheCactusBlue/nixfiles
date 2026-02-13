08acd290e644f2b6441def343ffb0ec9ef61ba60
TheCactusBlue <thecactusblue@gmail.com>
2026-02-13 14:51:50 -0800

    better headphones

--- home/pkgs/waybar/default.nix
+++ home/pkgs/waybar/default.nix
@@ -19,6 +19,7 @@
           "mpd"
           "idle_inhibitor"
           "pulseaudio"
+          "bluetooth"
           "network"
           "power-profiles-daemon"
           "cpu"
@@ -143,6 +144,16 @@
           };
         };
 
+        bluetooth = {
+          format = " {status}";
+          format-connected = " {device_alias}";
+          format-connected-battery = " {device_alias} {device_battery_percentage}%";
+          tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
+          tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
+        };
+
         network = {
           format-wifi = "{essid} ({signalStrength}%) ";
           format-ethernet = "{ipaddr}/{cidr} ";
@@ -154,7 +165,7 @@
 
         pulseaudio = {
           format = " {volume}%  {icon}    {format_source}";
-          format-bluetooth = "{volume}% {{icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
           format-bluetooth-muted = " {icon} {format_source}";
           format-muted = " {format_source}";
           format-source = "{volume}% ";
